90d5cec635b42cca9ec46f1a0affbea01afd9660
Gichie <jizhi.w77@foxmail.com>
2023-02-27 15:59:00 +0800

    00018-easy-tuple-length

---
+++ 00018-easy-tuple-length.ts
@@ -0,0 +1,22 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+
+type cases = [
+  Expect<Equal<Length<typeof tesla>, 4>>,
+  Expect<Equal<Length<typeof spaceX>, 5>>,
+  // @ts-expect-error
+  Length<5>,
+  // @ts-expect-error
+  Length<'hello world'>,
+]
+
+
+// ============= Your Code Here =============
+/**
+ * 1、T extends readonly any[] 构造一个新的只读的 any[]类型
+ * 2、length 关键字可以读取元组或者数组的长度
+ */
+type Length<T extends readonly any[]> = T['length']
