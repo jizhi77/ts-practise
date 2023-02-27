83d320c9868dc6644ec36fe0b477d4a6f4ab0a7d
Gichie <jizhi.w77@foxmail.com>
2023-02-27 19:58:52 +0800

    18142-medium-all

---
+++ 18142-medium-all.ts
@@ -0,0 +1,21 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<All<[1, 1, 1], 1>, true>>,
+  Expect<Equal<All<[1, 1, 2], 1>, false>>,
+  Expect<Equal<All<['1', '1', '1'], '1'>, true>>,
+  Expect<Equal<All<['1', '1', '1'], 1>, false>>,
+  Expect<Equal<All<[number, number, number], number>, true>>,
+  Expect<Equal<All<[number, number, string], number>, false>>,
+  Expect<Equal<All<[null, null, null], null>, true>>,
+  Expect<Equal<All<[[1], [1], [1]], [1]>, true>>,
+  Expect<Equal<All<[{}, {}, {}], {}>, true>>,
+]
+
+
+// ============= Your Code Here =============
+/**
+ * T[number]：遍历元组、数组类型的值
+ */
+type All<T extends any[], K> = T[number] extends K ? true : false;
