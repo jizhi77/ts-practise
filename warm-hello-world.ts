00cbb29b9c48ca0edf910497d433811ac6fe943e
Ning Wei <wangjizhi@growingio.com>
2023-02-27 15:02:47 +0800

    hello-world

---
+++ warm-hello-world.ts
@@ -0,0 +1,9 @@
+import type { Equal, Expect, NotAny } from '@type-challenges/utils'
+
+
+type HelloWorld = string;
+
+type cases = [
+    Expect<NotAny<HelloWorld>>,
+    Expect<Equal<HelloWorld, string>>,
+]
